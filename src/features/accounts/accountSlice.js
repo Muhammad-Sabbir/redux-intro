c4ad5f0078acd08b70cb7a0d133fc5e0e4f4eea1
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-11-09 12:29:48 +0600

    Chapter 20. Redux and Modern Redux Toolkit (With Thunks) >> Lecture: 16. Creating the Account Slice

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,61 +1,46 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
-export default function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposite":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      // LATER
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    case "account/convertingCurrency":
-      return { ...state, isLoading: true };
-    default:
-      return state;
-  }
-}
-export function deposit(amount, currency) {
-  if (currency === "USD") return { type: "account/deposite", payload: amount };
-  return async function (dispatch, getState) {
-    dispatch({ type: "account/convertingCurrency" });
-    // Api call
-    const host = "api.frankfurter.app";
-    const res = await fetch(
-      `https://${host}/latest?amount=${amount}&from=${currency}&to=USD`
-    );
-    const data = await res.json();
-    const converted = data.rates.USD;
-    dispatch({ type: "account/deposite", payload: converted });
-  };
-}
 
-export function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-export function requestLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: { amount, purpose },
-  };
-}
-export function payLoan() {
-  return { type: "account/payLoan" };
-}
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withDraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      // to receive more than one argument we added prepare() , that is why this is different than others
+
+      // there is another way to make it possible , we can pass the data as an object write in requestLoadn(), when well call it then.
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose }, // if we have more than two arguments then we have to add all the other argument in this payload
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+  },
+});
+
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export default accountSlice.reducer;
